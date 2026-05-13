fa49b6f421a5a971096bdc3b976204dbf3fa35a4
vajradevam <vajradevam@gmail.com>
2026-05-14 02:01:15 +0530

    Minimal Hello World test

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
-
 const PORT = process.env.PORT || 8765
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/debug') {
-    const info = [
-      '__dirname: ' + __dirname,
-      'cwd: ' + process.cwd(),
-      'files: ' + fs.readdirSync(__dirname).join(', '),
-      'cwd_files: ' + fs.readdirSync(process.cwd()).join(', '),
-    ]
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    return res.end(info.join('\n'))
-  }
-  if (req.url === '/' || req.url === '/index.html') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    return res.end('<h1>Hello from DecentChat</h1>')
-  }
-  res.writeHead(404)
-  res.end('Not found')
-})
-
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`)
-})
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end('<h1>Hello World</h1>')
+}).listen(PORT, () => console.log('on', PORT))
