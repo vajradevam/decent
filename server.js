6587ffd09dbfaab7271271a26226d324347fe54c
vajradevam <vajradevam@gmail.com>
2026-05-14 02:17:37 +0530

    Zero-dependency minimal server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const PORT = process.env.PORT || 8765
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end('<h1>Hello World</h1>')
-}).listen(PORT, () => console.log('on', PORT))
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('ok')
+}).listen(PORT, '0.0.0.0')
